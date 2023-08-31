f1771497eb0499287df3338dfdaa994a2b8d6cc9
Cláudio Azevedo <139291563+claudioazevedojr@users.noreply.github.com>
2023-08-31 10:32:09 -0300

    Add files via upload

---
+++ LP/JS/notasMedia.js
@@ -0,0 +1,12 @@
+
+
+n1=0
+n2=0
+n3=0
+m=0
+console.log('Digite as 3 notas: ');
+var n1 = number(prompt("Digite a primeira nota: "));
+var n2 = number(prompt("Digite a segunda nota: ")); 
+var n3 = number(prompt("Digite a terceira nota: "));
+var m= (n1+n2+n3)/3
+console.log(`A média do aluno é: ${m}`);
