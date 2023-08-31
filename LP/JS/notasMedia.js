22fe9a745f71a3ca4e8461720d46f47a7f033dc0
claudioazevedojr <claudioazevedojr@hotmail.com>
2023-08-31 10:43:11 -0300

    Update notasMedia.js
    versão 1.1

--- LP/JS/notasMedia.js
+++ LP/JS/notasMedia.js
@@ -5,8 +5,8 @@ n2=0
 n3=0
 m=0
 console.log('Digite as 3 notas: ');
-var n1 = number(prompt("Digite a primeira nota: "));
-var n2 = number(prompt("Digite a segunda nota: ")); 
-var n3 = number(prompt("Digite a terceira nota: "));
+var n1 = parseInt(prompt("Digite a primeira nota: "));
+var n2 = parseInt(prompt("Digite a segunda nota: ")); 
+var n3 = parseInt(prompt("Digite a terceira nota: "));
 var m= (n1+n2+n3)/3
 console.log(`A média do aluno é: ${m}`);
